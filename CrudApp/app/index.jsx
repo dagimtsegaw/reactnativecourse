25523dfc51b52a8bdd711f92a7929f1faa70ead5
dagimtsegaw <dagatse577@gmail.com>
2024-12-21 17:46:15 +0300

    lesson 4 - lesson 5

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -4,37 +4,140 @@ import {
   Text,
   View,
   StyleSheet,
+  Pressable,
   FlatList,
   TextInput,
   Button,
 } from "react-native";
+import { data } from "@/data/todos";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
 
 export default function Index() {
-  const [text, onChangeText] = useState("");
-  return (
-    <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <Button
-        title="Add"
-        style={styles.button}
-        onPress={(text) => {
-          <Text>{text}</Text>;
-        }}
-      />
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+  const [text, setText] = useState("");
+
+  const [loaded, error] = useFonts({ Inter_500Medium });
+
+  if (!loaded && !error) {
+    return null;
+  }
+  const addTodos = () => {
+    if (text.trim) {
+      const newId = todos.length > 1 ? todos[0].id + 1 : 1;
+      setTodos([{ title: text, id: newId, completed: false }, ...todos]);
+      setText("");
+    }
+  };
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const renderItem = ({ item }) => (
+    <View style={styles.todoItem}>
+      <Text
+        style={[styles.todoText, item.completed && styles.completedText]}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}
+      </Text>
+      <Pressable onPress={() => removeTodo(item.id)}>
+        <MaterialIcons
+          name="delete-forever"
+          size={36}
+          color="red"
+          selectable={undefined}
+        />
+      </Pressable>
     </View>
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add new todo"
+          placeholderTextColor="gray"
+          onChangeText={setText}
+          value={text}
+        />
+        <Pressable onPress={addTodos} style={styles.addButton}>
+          <Text style={styles.addButtonText}>Add</Text>
+        </Pressable>
+      </View>
+      <FlatList
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={(todo) => todo.id}
+        contentContainerStyle={{ flexGrow: 1 }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
     padding: 10,
+    width: "100%",
+    maxWidth: 1024,
+    marginHorizontal: "auto",
+  },
+  input: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    fontSize: 18,
+    fontFamily: "Inter_500Medium",
+    minWidth: 0,
+    color: "white",
+  },
+  addButton: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    padding: 10,
+  },
+  addButtonText: {
+    color: "black",
+    fontSize: 18,
+  },
+  todoItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 4,
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    width: "100%",
+    maxWidth: "1024",
+    marginHorizontal: "auto",
+    pointerEvents: "auto",
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 18,
+    fontFamily: "Inter_500Medium",
+    color: "white",
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    color: "gray",
   },
 });
